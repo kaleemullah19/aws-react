f5efeb62b308a09fda9cc5eba0baeb1675d77d19
kaleemullah19 <kaleemullah19@gmail.com>
2024-02-03 12:20:53 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
+import confident from './confident.mp4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* Render the video */}
+        <video width="620" height="360" controls>
+          <source src={confident} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Render some text */}
+        <h1>Welcome to My React App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a sample React application created by Kaleem and Shukrullah Khan.
         </p>
+
+        {/* Render a link */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +27,9 @@ function App() {
         >
           Learn React
         </a>
+
+        {/* Render the logo */}
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
