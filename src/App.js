d6dce09f729605da996ba784a5bb8fc14fea5c91
kaleemullah19 <kaleemullah19@gmail.com>
2024-02-03 23:59:47 -0500

    learning basic react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = (prev, curr) => {
+    console.log("prev", prev);
+    console.log("curr", curr);
+    setCount(count + 1);
+  };
+  const click = () => {
+    alert("ok");
+  };
+  const products = [
+    { title: "Cabbage", id: 1, isFruit: false },
+    { title: "Garlic", id: 2, isFruit: false },
+    { title: "Apple", id: 3, isFruit: true },
+  ];
+
+  const listItem = products.map((product) => (
+    <li style={{ color: product.isFruit ? "green" : "red" }} key={product.id}>
+      {product.title}
+    </li>
+  ));
   return (
     <div className="App">
       <header className="App-header">
         {/* Render the video */}
         <video width="620" height="360" controls>
-          <source src="https://confident-video.s3.amazonaws.com/confident.mp4" type="video/mp4"/>
+          <source
+            src="https://confident-video.s3.amazonaws.com/confident.mp4"
+            type="video/mp4"
+          />
           Your browser does not support the video tag.
         </video>
 
         {/* Render some text */}
+        <button onClick={handleCount}>Counter {count}</button>
         <h1>Welcome to My React App</h1>
         <p>
-          This is a sample React application created by Kaleem and Shukrullah Khan.
+          This is a sample React application created by Kaleem and Shukrullah
+          Khan.
         </p>
-
+        <button onClick={click}>Click here</button>
+        <ul>{listItem}</ul>
         {/* Render a link */}
         <a
           className="App-link"
